0b458848cbfaab7258896bc3f05aec2550ad54eb
riemsloh <olaf.lueg@gmail.com>
2025-06-14 10:43:34 +0200

    Initial Commit

---
+++ Wunderground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wunderground
+//
+//  Created by Olaf on 14.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wunderground/WundergroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  WundergroundApp.swift
+//  Wunderground
+//
+//  Created by Olaf on 14.06.25.
+//
+
+import SwiftUI
+
+@main
+struct WundergroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
